a3e517a02fa2c5b9b39baf04ba9aa2020cce011b
Azeez1 <sabaazeez12@gmail.com>
2025-05-26 15:53:19 -0400

    Add lead qualification utilities and integrate

--- src/services/email/campaign-manager.js
+++ src/services/email/campaign-manager.js
@@ -1,10 +1,23 @@
 const Queue = require('bull');
 const emailQueue = new Queue('email', process.env.REDIS_URL);
+const { isLeadQualified, calculateLeadScore, MINIMUM_SCORE_THRESHOLD } = require('../../utils/lead-quality');
+const logger = require('../../utils/logger');
 
 // Schedule a sequence of emails for a lead using Bull queue.
-async function scheduleCampaign(leadId, emails) {
+async function scheduleCampaign(lead, emails) {
+  if (!isLeadQualified(lead)) {
+    logger.warn('Lead failed qualification check, campaign not scheduled');
+    return null;
+  }
+
+  const score = calculateLeadScore(lead);
+  if (score < MINIMUM_SCORE_THRESHOLD) {
+    logger.warn(`Lead score ${score} below threshold, campaign not scheduled`);
+    return null;
+  }
+
   // TODO: push each email in the sequence to emailQueue.add()
-  return leadId;
+  return lead.id;
 }
 
 module.exports = { scheduleCampaign };
